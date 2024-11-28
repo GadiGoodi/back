7330f7589fe03812c92714312d949816a229a41e
jong1uk <106105315+jong1uk@users.noreply.github.com>
2024-11-28 12:44:34 +0900

    feat: UserCodeRoom 생성자 Builder, UserCodeRoomEntity from, toModel 메서드 추가

--- src/main/java/com/gagoo/thiscoding/domain/maria/usercoderoom/domain/UserCodeRoom.java
+++ src/main/java/com/gagoo/thiscoding/domain/maria/usercoderoom/domain/UserCodeRoom.java
@@ -2,13 +2,37 @@ package com.gagoo.thiscoding.domain.maria.usercoderoom.domain;
 
 import com.gagoo.thiscoding.domain.maria.coderoom.domain.CodeRoom;
 import com.gagoo.thiscoding.domain.maria.user.domain.User;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class UserCodeRoom {
-    private Long id;
-    private User user;
-    private CodeRoom codeRoom;
-    private boolean isActivated;
-    private boolean isAccepted;
+    private final Long id;
+    private final User user;
+    private final CodeRoom codeRoom;
+    private final boolean isActivated;
+    private final boolean isAccepted;
+
+    @Builder
+    public UserCodeRoom(Long id, User user, CodeRoom codeRoom, boolean isActivated, boolean isAccepted) {
+        this.id = id;
+        this.user = user;
+        this.codeRoom = codeRoom;
+        this.isActivated = isActivated;
+        this.isAccepted = isAccepted;
+    }
+
+    public UserCodeRoom accept() {
+        return UserCodeRoom.builder()
+            .id(id)
+            .user(user)
+            .codeRoom(codeRoom)
+            .isAccepted(true)
+            .isActivated(true)
+            .build();
+    }
+
+
+
+
 }
--- src/main/java/com/gagoo/thiscoding/domain/maria/usercoderoom/infrastructure/UserCodeRoomEntity.java
+++ src/main/java/com/gagoo/thiscoding/domain/maria/usercoderoom/infrastructure/UserCodeRoomEntity.java
@@ -2,6 +2,7 @@ package com.gagoo.thiscoding.domain.maria.usercoderoom.infrastructure;
 
 import com.gagoo.thiscoding.domain.maria.coderoom.infrastructure.CodeRoomEntity;
 import com.gagoo.thiscoding.domain.maria.user.infrastructure.UserEntity;
+import com.gagoo.thiscoding.domain.maria.usercoderoom.domain.UserCodeRoom;
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -27,5 +28,25 @@ public class UserCodeRoomEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "code_room_id")
     private CodeRoomEntity codeRoom;
+
+    public UserCodeRoom toModel() {
+        return UserCodeRoom.builder()
+            .id(this.id)
+            .isActivated(this.isActivated)
+            .isAccepted(this.isAccepted)
+            .user(this.user.toModel())
+            .codeRoom(this.codeRoom.toModel())
+            .build();
+    }
+
+    public static UserCodeRoomEntity from(UserCodeRoom userCodeRoom) {
+        UserCodeRoomEntity userCodeRoomEntity = new UserCodeRoomEntity();
+        userCodeRoomEntity.id = userCodeRoom.getId();
+        userCodeRoomEntity.isActivated = userCodeRoom.isActivated();
+        userCodeRoomEntity.isAccepted = userCodeRoom.isAccepted();
+        userCodeRoomEntity.user = UserEntity.from(userCodeRoom.getUser());
+        userCodeRoomEntity.codeRoom = CodeRoomEntity.from(userCodeRoom.getCodeRoom());
+        return userCodeRoomEntity;
+    }
 
 }
