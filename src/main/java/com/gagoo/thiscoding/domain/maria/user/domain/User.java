878f3ccf6da0bbfa7f7bd708f2a47a8a3157ab10
jong1uk <106105315+jong1uk@users.noreply.github.com>
2024-11-28 12:23:51 +0900

    feat: User Builder 생성자, UserEntity from, toModel 메서드 추가

--- src/main/java/com/gagoo/thiscoding/domain/maria/user/domain/User.java
+++ src/main/java/com/gagoo/thiscoding/domain/maria/user/domain/User.java
@@ -3,10 +3,11 @@ package com.gagoo.thiscoding.domain.maria.user.domain;
 import com.gagoo.thiscoding.domain.maria.user.domain.contants.Role;
 import com.gagoo.thiscoding.domain.maria.user.domain.contants.Social;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+//@AllArgsConstructor
 public class User {
 
     private final Long id;
@@ -17,4 +18,14 @@ public class User {
     private final Role role;
     private final Social social;
 
+    @Builder
+    public User (Long id, String email, String password, boolean isActivated, boolean isBanned, Role role, Social social) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.isActivated = isActivated;
+        this.isBanned = isBanned;
+        this.role = role;
+        this.social = social;
+    }
 }
--- src/main/java/com/gagoo/thiscoding/domain/maria/user/infrastructure/UserEntity.java
+++ src/main/java/com/gagoo/thiscoding/domain/maria/user/infrastructure/UserEntity.java
@@ -2,6 +2,7 @@ package com.gagoo.thiscoding.domain.maria.user.infrastructure;
 
 import com.gagoo.thiscoding.domain.maria.user.domain.contants.Social;
 import com.gagoo.thiscoding.domain.maria.user.domain.contants.Role;
+import com.gagoo.thiscoding.domain.maria.user.domain.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -29,5 +30,29 @@ public class UserEntity {
 
     @Enumerated(EnumType.STRING)
     private Social social;
+
+    public User toModel() {
+        return User.builder()
+            .id(this.id)
+            .email(this.email)
+            .password(this.password)
+            .isActivated(this.isActivated)
+            .isBanned(this.isBanned)
+            .role(this.role)
+            .social(this.social)
+            .build();
+    }
+    public static UserEntity from(User user) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.id = user.getId();
+        userEntity.email = user.getEmail();
+        userEntity.password = user.getPassword();
+        userEntity.isActivated = user.isActivated();
+        userEntity.isBanned = user.isBanned();
+        userEntity.role = user.getRole();
+        userEntity.social = user.getSocial();
+        return userEntity;
+
+    }
 
 }
