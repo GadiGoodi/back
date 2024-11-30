5cb39f5e624009e6d73bcef22fe1b07bd65c0861
Cro <junsj1230@naver.com>
2024-11-30 15:11:28 +0900

    feat : JWT 토큰 생성

---
+++ src/main/java/com/gagoo/thiscoding/global/config/security/JWTUtil.java
@@ -0,0 +1,67 @@
+package com.gagoo.thiscoding.global.config.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@PropertySource("classpath:config/application-jwt.properties")
+
+public class JWTUtil {
+
+    private SecretKey secretKey;
+
+    public JWTUtil(@Value("{spring.jwt.key}") String secret) {
+        this.secretKey =
+                new SecretKeySpec(
+                        secret.getBytes(StandardCharsets.UTF_8),
+                        Jwts.SIG.HS256.key().build().getAlgorithm()
+                );
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+
+    public String createJwt(String username, String role, Long expiredMs) {
+
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
