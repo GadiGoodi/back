a31c7438f10fcae38706726780bc5e449c11c40b
nameisris <insoo5110@gmail.com>
2024-11-30 15:20:05 +0900

    chore: webSocket 관련 config 파일을 websocket 패키지로 묶음

--- src/main/java/com/gagoo/thiscoding/global/config/websocket/WebConfig.java
+++ src/main/java/com/gagoo/thiscoding/global/config/websocket/WebConfig.java
@@ -1,4 +1,4 @@
-package com.gagoo.thiscoding.global.config;
+package com.gagoo.thiscoding.global.config.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
