94dac5e5dbe4ca0a640e6eb6b16b86c4c4cc1fe7
canelo <chyg201672@gmail.com>
2024-12-01 20:06:49 +0900

    feat:관리자 공지사항 Domain,Entity 수정코드
    관리자 공지사항 CRUD에 맞게 도메인과 엔티티 수정했습니다.

--- src/main/java/com/gagoo/thiscoding/domain/maria/manager/service/port/ManagerRepository.java
+++ src/main/java/com/gagoo/thiscoding/domain/maria/manager/service/port/ManagerRepository.java
@@ -1,4 +1,17 @@
 package com.gagoo.thiscoding.domain.maria.manager.service.port;
 
+import com.gagoo.thiscoding.domain.maria.manager.domain.Manager;
+import com.gagoo.thiscoding.domain.maria.manager.infrastructure.ManagerEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
 public interface ManagerRepository {
+
+    Page<ManagerEntity> findAll(Pageable pageable);
+    ManagerEntity save(ManagerEntity manager);
+    void deleteById(Long id);
+    Optional<ManagerEntity> findById(Long id);
 }
+
