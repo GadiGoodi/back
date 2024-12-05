a4495cff3b7323d563b13375c0e8d030d703a138
nameisris <insoo5110@gmail.com>
2024-12-06 00:38:28 +0900

    feat: Code 생성 기능에 따른 CodeDocument 변경

--- src/main/java/com/gagoo/thiscoding/domain/mongo/code/infrastructure/CodeDocument.java
+++ src/main/java/com/gagoo/thiscoding/domain/mongo/code/infrastructure/CodeDocument.java
@@ -1,5 +1,6 @@
 package com.gagoo.thiscoding.domain.mongo.code.infrastructure;
 
+import com.gagoo.thiscoding.domain.mongo.code.domain.Code;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -23,8 +24,30 @@ public class CodeDocument {
     @Field(name = "file_name")
     private String fileName;
 
-    private String path;
+    private String value;
 
     @Field(name = "save_date")
     private LocalDateTime saveDate;
+
+    public Code toModel() {
+        return Code.builder()
+                .id(this.id)
+                .roomId(this.roomId)
+                .writerId(this.writerId)
+                .fileName(this.fileName)
+                .value(this.value)
+                .saveDate(this.saveDate)
+                .build();
+    }
+
+    public static CodeDocument from(Code code) {
+        CodeDocument codeDocument = new CodeDocument();
+        codeDocument.id = code.getId();
+        codeDocument.roomId = code.getRoomId();
+        codeDocument.writerId = code.getWriterId();
+        codeDocument.fileName = code.getFileName();
+        codeDocument.value = code.getValue();
+        codeDocument.saveDate = code.getSaveDate();
+        return codeDocument;
+    }
 }
