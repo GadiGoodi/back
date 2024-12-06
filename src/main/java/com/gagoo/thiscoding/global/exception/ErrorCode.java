80794f9dc01ee1d31f5e37ac3476000a751de8d5
nameisris <insoo5110@gmail.com>
2024-12-06 13:24:02 +0900

    fix: ErrorCode 내 CODE_ROOM_NOT_FOUND 추가

--- src/main/java/com/gagoo/thiscoding/global/exception/ErrorCode.java
+++ src/main/java/com/gagoo/thiscoding/global/exception/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     PASSWORD_NOT_EQUAL(HttpStatus.BAD_REQUEST, "패스워드가 일치하지 않습니다."),
     ALREADY_CREATE_EMAIL(HttpStatus.CONFLICT, "이미 가입된 이메일입니다."),
     EXIST_MEMBER_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 이메일입니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 이메일입니다."),
+    CODE_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 코드방입니다.");
 
     private final HttpStatus status;
     private final String message;
