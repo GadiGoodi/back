f7f4a10ba60731f4e4e5598902310a6c07a37b86
jong1uk <106105315+jong1uk@users.noreply.github.com>
2024-12-20 01:59:34 +0900

    Update ErrorCode.java

--- src/main/java/com/gagoo/thiscoding/global/exception/ErrorCode.java
+++ src/main/java/com/gagoo/thiscoding/global/exception/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
     ALREADY_FRIEND(HttpStatus.CONFLICT, "이미 친구목록에 존재하는 회원입니다."),
     ALREADY_USER_CODE_ROOM(HttpStatus.CONFLICT, "이미 참여중인 코드방입니다."),
     EXIST_MEMBER_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
+    OVER_CAPACITY_CODE_ROOM(HttpStatus.CONFLICT, "인원 수가 초과되었습니다."),
     USER_CODE_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 코드방입니다."),
     CODE_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 코드방입니다."),
     CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 채팅방입니다."),
@@ -25,8 +26,8 @@ public enum ErrorCode {
     INVALID_SIGNATURE(HttpStatus.UNAUTHORIZED, "잘못된 JWT 서명입니다."),
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "토큰 정보가 올바르지 않습니다"),
-    TOKEN_NOT_EQUALS(HttpStatus.UNAUTHORIZED, "토큰 정보가 올바르지 않습니다"),
-    OVER_CAPACITY_CODE_ROOM(HttpStatus.CONFLICT, "인원 수가 초과되었습니다.");
+    TOKEN_NOT_EQUALS(HttpStatus.UNAUTHORIZED, "토큰 정보가 올바르지 않습니다");
+    
 
     private final HttpStatus status;
     private final String message;
