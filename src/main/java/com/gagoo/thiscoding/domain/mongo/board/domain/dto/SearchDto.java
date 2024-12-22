8ad90df2bc4b306a3ed7df2c92d7e21dec251d40
jong1uk <106105315+jong1uk@users.noreply.github.com>
2024-12-22 17:19:34 +0900

    feat: qna 기본 검색 기능

---
+++ src/main/java/com/gagoo/thiscoding/domain/mongo/board/domain/dto/SearchDto.java
@@ -0,0 +1,40 @@
+package com.gagoo.thiscoding.domain.mongo.board.domain.dto;
+
+import com.gagoo.thiscoding.domain.mongo.board.domain.Board;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SearchDto {
+    private String id;
+    private Long userId;
+    private String title;
+    private String content;
+    private String language;
+    private Long parentId;
+    private Long likeCount;
+    private Long viewCount;
+    private Long answerCount;
+    private boolean isBlind;
+    private boolean isSelected;
+    private LocalDateTime createDate;
+
+    public static SearchDto from(Board board) {
+        return SearchDto.builder()
+            .id(board.getId())
+            .userId(board.getUserId())
+            .title(board.getTitle())
+            .content(board.getContent())
+            .language(board.getLanguage())
+            .parentId(board.getParentId())
+            .likeCount(board.getLikeCount())
+            .viewCount(board.getViewCount())
+            .answerCount(board.getAnswerCount())
+            .isBlind(board.isBlind())
+            .isSelected(board.isSelected())
+            .createDate(board.getCreateDate())
+            .build();
+    }
+}
