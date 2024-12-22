24a055caa19f9a509994f426d3552c476a7f437f
nameisris <insoo5110@gmail.com>
2024-12-22 16:09:22 +0900

    feat: 코드방 입장 관련 Controller, Service, DTO, Response 추가

---
+++ src/main/java/com/gagoo/thiscoding/domain/maria/coderoom/domain/dto/CodeRoomEnter.java
@@ -0,0 +1,35 @@
+package com.gagoo.thiscoding.domain.maria.coderoom.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class CodeRoomEnter {
+    @NotBlank
+    private final Long roomId;
+    @NotBlank
+    private final String language;
+    @NotBlank
+    private final String value;
+    @NotBlank
+    private final String codeId;
+
+    @Builder
+    public CodeRoomEnter(Long roomId, String language, String value, String codeId) {
+        this.roomId = roomId;
+        this.language = language;
+        this.value = value;
+        this.codeId = codeId;
+    }
+
+    public static CodeRoomEnter of(Long roomId, String language, String value, String codeId) {
+        return CodeRoomEnter.builder()
+                .roomId(roomId)
+                .language(language)
+                .value(value)
+                .codeId(codeId)
+                .build();
+    }
+
+}
--- src/main/java/com/gagoo/thiscoding/domain/maria/coderoom/service/CodeRoomServiceImpl.java
+++ src/main/java/com/gagoo/thiscoding/domain/maria/coderoom/service/CodeRoomServiceImpl.java
@@ -3,8 +3,12 @@ package com.gagoo.thiscoding.domain.maria.coderoom.service;
 import com.gagoo.thiscoding.domain.maria.coderoom.controller.port.CodeRoomService;
 import com.gagoo.thiscoding.domain.maria.coderoom.domain.CodeRoom;
 import com.gagoo.thiscoding.domain.maria.coderoom.domain.dto.CodeRoomCreate;
+import com.gagoo.thiscoding.domain.maria.coderoom.domain.dto.CodeRoomEnter;
 import com.gagoo.thiscoding.domain.maria.coderoom.service.exception.CodeRoomNotFoundException;
 import com.gagoo.thiscoding.domain.maria.coderoom.service.port.CodeRoomRepository;
+import com.gagoo.thiscoding.domain.mongo.code.domain.Code;
+import com.gagoo.thiscoding.domain.mongo.code.service.exception.CodeNotFoundException;
+import com.gagoo.thiscoding.domain.mongo.code.service.port.CodeRepository;
 import com.gagoo.thiscoding.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,9 +20,13 @@ import java.util.UUID;
 public class CodeRoomServiceImpl implements CodeRoomService {
 
     private final CodeRoomRepository codeRoomRepository;
+    private final CodeRepository codeRepository;
 
-    // 코드방 생성
-    // return 생성된 codeRoom 정보
+    /**
+     * 코드방 생성
+     * @param codeRoomCreate
+     * @return 생성한 CodeRoom
+     */
     @Override
     public CodeRoom createCodeRoom(CodeRoomCreate codeRoomCreate) {
         CodeRoom codeRoom = CodeRoom.create(codeRoomCreate);
@@ -26,12 +34,32 @@ public class CodeRoomServiceImpl implements CodeRoomService {
         return codeRoomRepository.save(codeRoom);
     }
 
-    // 코드방 입장
-    // return 입장한 codeRoom 정보
+    /**
+     * 코드방 입장
+     * @param uuid
+     * @return 입장한 CodeRoom
+     */
     @Override
-    public CodeRoom enterCodeRoom(UUID uuid) {
-        return codeRoomRepository.findByUuid(uuid).orElseThrow(
+    public CodeRoomEnter enterCodeRoom(UUID uuid) {
+        CodeRoom codeRoom = codeRoomRepository.findByUuid(uuid).orElseThrow(
                 () -> new CodeRoomNotFoundException(ErrorCode.CODE_ROOM_NOT_FOUND)
+        );
+
+        Code code = getCodeByRoomIdAndFileName(codeRoom.getId(), "main");
+
+        return CodeRoomEnter.of(codeRoom.getId(), codeRoom.getLanguage(), code.getValue(), code.getId());
+    }
+
+    /**
+     * 코드 조회
+     * @param roomId
+     * @param fileName
+     * @return 조회한 Code
+     */
+    @Override
+    public Code getCodeByRoomIdAndFileName(Long roomId, String fileName) {
+        return codeRepository.findByRoomIdAndFileName(roomId, fileName).orElseThrow(
+                () -> new CodeNotFoundException(ErrorCode.CODE_NOT_FOUND)
         );
     }
 
