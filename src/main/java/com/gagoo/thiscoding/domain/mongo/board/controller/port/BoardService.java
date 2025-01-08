fc6d648f9a491af734268ae520af0b72799011c0
canelo <chyg201672@gmail.com>
2025-01-08 17:38:00 +0900

    fix: User 패키지에서 Board 패키지로 변경
    기존 마이페이지 작성한 QnA 목록 Board 패키지로 코드 이동

--- src/main/java/com/gagoo/thiscoding/domain/mongo/board/controller/port/BoardService.java
+++ src/main/java/com/gagoo/thiscoding/domain/mongo/board/controller/port/BoardService.java
@@ -4,8 +4,10 @@ import com.gagoo.thiscoding.domain.mongo.board.domain.Board;
 import com.gagoo.thiscoding.domain.mongo.board.domain.dto.BoardCreate;
 import com.gagoo.thiscoding.domain.mongo.board.domain.dto.Search;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface BoardService {
     Board create(BoardCreate boardCreate);
     Page<Search> searchByKeyword(String keyword, int page);
+    Page<Board> getMyPagePostQnA(Pageable pageable);
 }
--- src/main/java/com/gagoo/thiscoding/domain/mongo/board/service/BoardServiceImpl.java
+++ src/main/java/com/gagoo/thiscoding/domain/mongo/board/service/BoardServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gagoo.thiscoding.domain.mongo.board.service;
 
+import com.gagoo.thiscoding.domain.maria.user.controller.port.UserService;
+import com.gagoo.thiscoding.domain.maria.user.domain.User;
 import com.gagoo.thiscoding.domain.mongo.board.controller.port.BoardService;
 import com.gagoo.thiscoding.domain.mongo.board.domain.Board;
 import com.gagoo.thiscoding.domain.mongo.board.domain.dto.BoardCreate;
@@ -7,8 +9,10 @@ import com.gagoo.thiscoding.domain.mongo.board.domain.dto.Search;
 import com.gagoo.thiscoding.domain.mongo.board.service.port.BoardRepository;
 import com.gagoo.thiscoding.global.paging.PageSize;
 import com.gagoo.thiscoding.global.paging.PagingProcessor;
+import com.gagoo.thiscoding.global.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class BoardServiceImpl implements BoardService {
 
     private final BoardRepository boardRepository;
+    private final UserService userService;
 
     /**
      * qna 등록
@@ -33,5 +38,11 @@ public class BoardServiceImpl implements BoardService {
     @Override
     public Page<Search> searchByKeyword(String keyword, int page) {
         return boardRepository.findByTitleOrContent(keyword, keyword, PagingProcessor.toPageable(page, PageSize.QNA));
+    }
+
+    @Override
+    public Page<Board> getMyPagePostQnA(Pageable pageable) {
+        User currentUser = userService.getByEmail(SecurityUtils.getUserEmail());
+        return boardRepository.findByUserId(currentUser.getId(), pageable);
     }
 }
