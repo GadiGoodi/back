912efecacb3dde1ac21045d176b73865bc5e61f7
nameisris <insoo5110@gmail.com>
2025-01-09 16:20:46 +0900

    fix: CodeRoom, UserCodeRoom, Code Controller 내 사용자 인증 코드 추가

--- src/main/java/com/gagoo/thiscoding/domain/maria/coderoom/controller/CodeRoomController.java
+++ src/main/java/com/gagoo/thiscoding/domain/maria/coderoom/controller/CodeRoomController.java
@@ -3,6 +3,8 @@ package com.gagoo.thiscoding.domain.maria.coderoom.controller;
 import com.gagoo.thiscoding.domain.maria.coderoom.controller.port.CodeRoomService;
 import com.gagoo.thiscoding.domain.maria.coderoom.controller.response.CodeRoomEnterResponse;
 import com.gagoo.thiscoding.domain.maria.coderoom.domain.dto.CodeRoomEnter;
+import com.gagoo.thiscoding.domain.maria.user.domain.contants.Role;
+import com.gagoo.thiscoding.global.security.aop.AuthorizationRequired;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/codingrooms")
@@ -21,6 +25,7 @@ public class CodeRoomController {
 
     // 코드방 입장 (조회)
     @GetMapping("/{uuid}")
+    @AuthorizationRequired(value = Role.USER, status = OK)
     public ResponseEntity<CodeRoomEnterResponse> enterCodeRoom(@PathVariable String uuid) {
         UUID uuidObject = UUID.fromString(uuid);
         CodeRoomEnter codeRoomEnter = codeRoomService.enterCodeRoom(uuidObject);
--- src/main/java/com/gagoo/thiscoding/domain/maria/coderoom/controller/CodeRoomCreateController.java
+++ src/main/java/com/gagoo/thiscoding/domain/maria/coderoom/controller/CodeRoomCreateController.java
@@ -4,6 +4,8 @@ import com.gagoo.thiscoding.domain.maria.coderoom.controller.port.CodeRoomServic
 import com.gagoo.thiscoding.domain.maria.coderoom.controller.response.CodeRoomCreateResponse;
 import com.gagoo.thiscoding.domain.maria.coderoom.domain.CodeRoom;
 import com.gagoo.thiscoding.domain.maria.coderoom.domain.dto.CodeRoomCreate;
+import com.gagoo.thiscoding.domain.maria.user.domain.contants.Role;
+import com.gagoo.thiscoding.global.security.aop.AuthorizationRequired;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("api/codingrooms-modal")
@@ -21,6 +25,7 @@ public class CodeRoomCreateController {
 
     // 코드방 생성
     @PostMapping
+    @AuthorizationRequired(value = Role.USER, status = OK)
     public ResponseEntity<CodeRoomCreateResponse> createCodeRoom(@RequestBody CodeRoomCreate codeRoomCreate) {
         CodeRoom codeRoom = codeRoomService.createCodeRoom(codeRoomCreate);
 
