7b77ee5363fc9ba2cf99c6863f11384eb3c370bd
Cro <junsj1230@naver.com>
2025-01-19 23:16:59 +0900

    test : 잘못된 이메일, 인증코드로 요청시 발생하는 에러 테스트 추가

--- src/test/java/com/gagoo/thiscoding/domain/maria/user/service/CertificationServiceImplTest.java
+++ src/test/java/com/gagoo/thiscoding/domain/maria/user/service/CertificationServiceImplTest.java
@@ -1,11 +1,14 @@
 package com.gagoo.thiscoding.domain.maria.user.service;
 
 import com.gagoo.thiscoding.domain.maria.user.domain.dto.JoinCode;
+import com.gagoo.thiscoding.domain.maria.user.service.exception.JoinCodeNotFoundException;
+import com.gagoo.thiscoding.domain.maria.user.service.exception.JoinCodeNotMatchException;
 import com.gagoo.thiscoding.domain.mock.TestContainer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CertificationServiceImplTest {
 
@@ -53,6 +56,42 @@ class CertificationServiceImplTest {
         // then
         assertThat(sendJoinCode.getEmail()).isEqualTo(result.getEmail());
         assertThat(sendJoinCode.getCode()).isEqualTo(result.getCode());
+    }
+
+    @Test
+    void 잘못된_인증코드_입력시_예외가_발생한다() {
+        // given
+        String email = "junsj1230@naver.com";
+        certificationService.sendJoinCode(email);
+
+        // when
+        JoinCode wrongJoinCode = JoinCode.builder()
+                .email(email)
+                .code("000000") // 잘못된 코드
+                .build();
+
+        // then
+        assertThrows(JoinCodeNotMatchException.class,
+                () -> certificationService.checkJoinCode(wrongJoinCode)
+        );
+    }
+
+    @Test
+    void 잘못된_이메일로_인증코드_검증을_요청시_예외가_발생한다() {
+        // given
+        String email = "junsj1230@naver.com";
+        JoinCode sendJoinCode = certificationService.sendJoinCode(email);
+
+        // when
+        String wrongEmail = "worng@naver.com";
+
+        JoinCode wrongJoinCode = JoinCode.builder()
+                .email(wrongEmail)
+                .code(sendJoinCode.getCode())
+                .build();
+
+        assertThrows(JoinCodeNotFoundException.class,
+                () -> certificationService.checkJoinCode(wrongJoinCode));
     }
 
 }
--- src/test/java/com/gagoo/thiscoding/domain/mock/FakeMailSender.java
+++ src/test/java/com/gagoo/thiscoding/domain/mock/FakeMailSender.java
@@ -3,28 +3,20 @@ package com.gagoo.thiscoding.domain.mock;
 import com.gagoo.thiscoding.domain.maria.user.domain.dto.Certification;
 import com.gagoo.thiscoding.domain.maria.user.service.port.MailSender;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class FakeMailSender implements MailSender {
 
-    public Map<String, String> javaMailSender = new HashMap<>();
-
     @Override
     public Certification send(String email) {
-        String code = generateRandomCode();
-
-        javaMailSender.put("subject", "thiscoding 회원가입 메일 인증 코드");
-        javaMailSender.put("email", email);
-        javaMailSender.put("joinCode", code);
-
-        return new Certification(javaMailSender.get("email"), javaMailSender.get("joinCode"));
+        return new Certification(email, generateSecureCode());
     }
 
-    private String generateRandomCode() {
-        int code = (int) (Math.random() * 1000000);
-        return String.format("%06d", code);
-    }
+    private String generateSecureCode() {
+        SecureRandom secureRandom = new SecureRandom();
 
+        return String.valueOf(
+                secureRandom.nextInt(900000) + 100000
+        );
+    }
 }
