78f782e194e1888cb7d2799ede01053d618f6c1c
Cro <junsj1230@naver.com>
2025-01-19 20:40:46 +0900

    refactor(test) : 인증코드 발급과 확인 리팩토링
    테스트코드에서 인증코드 발급할 때 고정된 값을 발급하여 검사하는 것이 아닌 동적으로 값을 생성하도록 변경
    
    동적으로 발급된 인증코드와 일치하는지 테스트 하도록 변경

--- src/test/java/com/gagoo/thiscoding/domain/mock/FakeMailSender.java
+++ src/test/java/com/gagoo/thiscoding/domain/mock/FakeMailSender.java
@@ -5,6 +5,7 @@ import com.gagoo.thiscoding.domain.maria.user.service.port.MailSender;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class FakeMailSender implements MailSender {
 
@@ -12,12 +13,18 @@ public class FakeMailSender implements MailSender {
 
     @Override
     public Certification send(String email) {
-        String code = "123456";
+        String code = generateRandomCode();
 
         javaMailSender.put("subject", "thiscoding 회원가입 메일 인증 코드");
         javaMailSender.put("email", email);
-        javaMailSender.put("joinCode", "123456");
+        javaMailSender.put("joinCode", code);
 
         return new Certification(javaMailSender.get("email"), javaMailSender.get("joinCode"));
     }
+
+    private String generateRandomCode() {
+        int code = (int) (Math.random() * 1000000);
+        return String.format("%06d", code);
+    }
+
 }
