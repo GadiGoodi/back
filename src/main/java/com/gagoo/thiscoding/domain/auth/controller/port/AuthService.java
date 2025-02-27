e03b87cea3d7fbed2fad2da873ab681b59c6a891
Cro <junsj1230@naver.com>
2025-02-27 13:48:22 +0900

    feat : 비밀번호 변경, 초기화

--- src/main/java/com/gagoo/thiscoding/domain/auth/controller/port/AuthService.java
+++ src/main/java/com/gagoo/thiscoding/domain/auth/controller/port/AuthService.java
@@ -1,8 +1,14 @@
 package com.gagoo.thiscoding.domain.auth.controller.port;
 
+import com.gagoo.thiscoding.domain.auth.controller.request.ChangePasswordRequest;
 import com.gagoo.thiscoding.domain.auth.controller.request.LoginRequest;
+import com.gagoo.thiscoding.domain.auth.controller.request.ResetPasswordRequest;
 import com.gagoo.thiscoding.domain.auth.dto.LoginDto;
+import com.gagoo.thiscoding.domain.maria.user.domain.User;
 
 public interface AuthService {
     LoginDto login(LoginRequest loginRequest);
+    User getUserInfo();
+    User changePassword(ChangePasswordRequest request);
+    User resetPassword(ResetPasswordRequest request);
 }
