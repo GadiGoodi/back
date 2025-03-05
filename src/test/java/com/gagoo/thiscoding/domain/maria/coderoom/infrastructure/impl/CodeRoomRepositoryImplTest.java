697bc4f7b1023de5e6f79fe6ac8d8640d2248c73
nameisris <insoo5110@gmail.com>
2025-03-06 01:11:34 +0900

    test: CodeRoomRepositoryImplTest 통합 테스트 코드 작성

---
+++ src/test/java/com/gagoo/thiscoding/domain/maria/coderoom/infrastructure/impl/CodeRoomRepositoryImplTest.java
@@ -0,0 +1,107 @@
+package com.gagoo.thiscoding.domain.maria.coderoom.infrastructure.impl;
+
+import com.gagoo.thiscoding.ThiscodingApplication;
+import com.gagoo.thiscoding.domain.maria.coderoom.domain.CodeRoom;
+import com.gagoo.thiscoding.domain.maria.coderoom.infrastructure.jpa.CodeRoomCustomRepository;
+import com.gagoo.thiscoding.domain.maria.user.domain.User;
+import com.gagoo.thiscoding.domain.maria.user.domain.contants.Role;
+import com.gagoo.thiscoding.domain.maria.user.domain.contants.Social;
+import com.gagoo.thiscoding.domain.maria.usercoderoom.domain.UserCodeRoom;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = ThiscodingApplication.class)
+@Transactional
+@ActiveProfiles(profiles = {"test"})
+@Sql(scripts = "/sql/coderoom-test-data.sql")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(locations = "classpath:config/application-test.yml")
+class CodeRoomRepositoryImplTest {
+    @Autowired
+    private CodeRoomCustomRepository codeRoomCustomRepository;
+
+    @Nested
+    @DisplayName("(참여 중인) 코드방으로 참여자 목록 조회")
+    class UserListGetUnitTest {
+        @Test
+        @DisplayName("(참여 중인) 코드방으로 참여자 목록을 조회할 수 있다.")
+        void findUserListByUserCodeRoom_참여_중인_코드방으로_참여자_목록_조회_성공() {
+            // given
+            User user01 = User.builder()
+                    .id(1L)
+                    .email("test01@test.com")
+                    .nickname("test01")
+                    .isActivated(true)
+                    .isBanned(false)
+                    .role(Role.USER)
+                    .social(Social.THIS_CODING)
+                    .build();
+
+            CodeRoom codeRoom = CodeRoom.builder()
+                    .id(1L)
+                    .uuid("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa")
+                    .title("teach me")
+                    .content("how to print the hello world?")
+                    .language("java")
+                    .headCount(5)
+                    .build();
+
+            UserCodeRoom userCodeRoom = UserCodeRoom.builder()
+                    .id(1L)
+                    .user(user01)
+                    .codeRoom(codeRoom)
+                    .isActivated(true)
+                    .build();
+
+            // when
+            List<User> userList = codeRoomCustomRepository.findUserListByUserCodeRoom(userCodeRoom);
+
+            // then
+            assertThat(userList).isNotEmpty();
+            assertThat(userList.size()).isEqualTo(5);
+            assertThat(userList).anyMatch(user -> user.getId().equals(user01.getId()));
+
+        }
+    }
+
+    @Nested
+    @DisplayName("사용자로 (참여 중인) 코드방 목록 조회")
+    class UserCodeRoomPageGetUnitTest {
+        @Test
+        @DisplayName("사용자로 (참여 중인) 코드방 목록을 조회할 수 있다.")
+        void findAllUserCodeRoomByUser_사용자로_참여_중인_코드방_목록_조회_성공() {
+            // given
+            Pageable pageable = PageRequest.of(0, 10);
+            User user = User.builder()
+                    .id(1L)
+                    .email("test01@test.com")
+                    .nickname("test01")
+                    .isActivated(true)
+                    .isBanned(false)
+                    .role(Role.USER)
+                    .social(Social.THIS_CODING)
+                    .build();
+
+            // when
+            Page<UserCodeRoom> result = codeRoomCustomRepository.findAllUserCodeRoomByUser(user, pageable);
+
+            // then
+            assertThat(result.getTotalElements()).isEqualTo(1);
+        }
+    }
+}
---
+++ src/test/resources/sql/coderoom-test-data.sql
@@ -0,0 +1,56 @@
+-- SET REFERENTIAL_INTEGRITY TRUE;
+DROP TABLE users CASCADE;
+DROP TABLE user_code_room CASCADE;
+DROP TABLE code_room CASCADE;
+
+-- 테이블 생성
+CREATE TABLE users (
+                       users_id BIGINT PRIMARY KEY,
+                       email VARCHAR(255),
+                       image_url VARCHAR(255),
+                       is_activated BOOLEAN NOT NULL,
+                       is_banned BOOLEAN NOT NULL,
+                       nickname VARCHAR(255),
+                       password VARCHAR(255),
+                       role ENUM('ADMIN', 'USER') DEFAULT 'USER',
+                       social ENUM('NAVER', 'THIS_CODING') DEFAULT 'THIS_CODING'
+);
+
+CREATE TABLE code_room (
+                           code_room_id BIGINT PRIMARY KEY,
+                           content VARCHAR(255),
+                           head_count INT,
+                           language VARCHAR(255),
+                           title VARCHAR(255),
+                           uuid VARCHAR(255)
+);
+
+CREATE TABLE user_code_room (
+                                user_code_room_id BIGINT PRIMARY KEY,
+                                is_activated BOOLEAN NOT NULL,
+                                users_id BIGINT,
+                                code_room_id BIGINT,
+                                foreign key (users_id) references users(users_id),
+                                foreign key (code_room_id) references code_room(code_room_id)
+);
+
+-- 더미 데이터 삽입
+INSERT INTO users (users_id, email, password, nickname, image_url, is_activated, is_banned, role, social)
+VALUES
+    (1, 'test01@test.com', 'pw01', 'test01', 'http://example.com/image1.png', true, false, 'USER', 'THIS_CODING'),
+    (2, 'test02@test.com', 'pw02', 'test02', 'http://example.com/image2.png', true, false, 'USER', 'NAVER'),
+    (3, 'test03@test.com', 'pw03', 'test03', 'http://example.com/image3.png', true, false, 'USER', 'NAVER'),
+    (4, 'test04@test.com', 'pw04', 'test04', 'http://example.com/image4.png', true, false, 'USER', 'THIS_CODING'),
+    (5, 'test05@test.com', 'pw05', 'test05', 'http://example.com/image5.png', true, false, 'ADMIN', 'THIS_CODING');
+
+INSERT INTO code_room (code_room_id, uuid, title, content, language, head_count)
+VALUES
+    (1, 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'teach me', 'how to print the hello world?', 'Java', 5);
+
+INSERT INTO user_code_room (user_code_room_id, users_id, code_room_id, is_activated)
+VALUES
+    (1, 1, 1, true),
+    (2, 2, 1, true),
+    (3, 3, 1, true),
+    (4, 4, 1, true),
+    (5, 5, 1, true);
