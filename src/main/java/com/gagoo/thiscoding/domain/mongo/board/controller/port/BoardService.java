613a5317e7973bb3e1f975201a22542eb87ea882
Cro <junsj1230@naver.com>
2025-03-13 23:43:56 +0900

    refactor : 방문자 식별 코드 분리
    VisitorIdProvider 구현하여 서비스 계층에서 Http 관련 코드 삭제

--- src/main/java/com/gagoo/thiscoding/domain/mongo/board/controller/port/BoardService.java
+++ src/main/java/com/gagoo/thiscoding/domain/mongo/board/controller/port/BoardService.java
@@ -8,16 +8,12 @@ import com.gagoo.thiscoding.domain.mongo.board.service.dto.AnswerList;
 import com.gagoo.thiscoding.domain.mongo.board.service.dto.QnaDetail;
 import com.gagoo.thiscoding.domain.mongo.board.service.dto.QnaList;
 import com.gagoo.thiscoding.global.paging.dto.CustomPageDto;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface BoardService {
     Board create(BoardCreate boardCreate);
-    QnaDetail get(String qnaId, HttpServletRequest request, HttpServletResponse response);
+    QnaDetail get(String qnaId, String visitorId);
     Board writeAnswer(String qnaId, BoardAnswer boardAnswer);
     Page<Search> searchByKeyword(String keyword, Pageable pageable);
     Page<Board> getMyPagePostQnA(Pageable pageable);
