a91008b986f663c19c3230df70dd4bfd0a089b63
Cro <junsj1230@naver.com>
2025-03-14 11:12:25 +0900

    fix : 비밀번호 변경 에러 해결
    비밀번호 변경할 때 기존 비밀번호를 그대로 변경하여 변경이 안 되는 에러 해결

--- src/main/java/com/gagoo/thiscoding/domain/auth/controller/request/ResetPasswordRequest.java
+++ src/main/java/com/gagoo/thiscoding/domain/auth/controller/request/ResetPasswordRequest.java
@@ -1,6 +1,7 @@
 package com.gagoo.thiscoding.domain.auth.controller.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record ResetPasswordRequest(@NotBlank String newPassword, @NotBlank String checkPassword) {
+public record ResetPasswordRequest(@NotBlank @Email String email, @NotBlank String newPassword, @NotBlank String checkPassword) {
 }
