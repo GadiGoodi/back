7de46c81c524ff09dad6322fc9e54ef17e03e794
Cro <junsj1230@naver.com>
2025-03-16 23:48:23 +0900

    fix : test 파일 인식 불가 해결
    test 실행시 ThiscodingApplicationTests에서 contextLoads에 올라가지 않아 @ActiveProfiles("test") 명시

--- src/test/java/com/gagoo/thiscoding/ThiscodingApplicationTests.java
+++ src/test/java/com/gagoo/thiscoding/ThiscodingApplicationTests.java
@@ -2,8 +2,10 @@ package com.gagoo.thiscoding;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ThiscodingApplicationTests {
 
     @Test
