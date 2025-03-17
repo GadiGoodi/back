3bee8314af22d81f34ebe208b6d51ff07698d6fc
Cro <junsj1230@naver.com>
2025-03-18 03:01:53 +0900

    fix : mailsender config path update
    서브모듈 적용 방법 변경으로 인한 PropertySource 환경변수 위치 변경

--- src/main/java/com/gagoo/thiscoding/global/config/MailSenderConfig.java
+++ src/main/java/com/gagoo/thiscoding/global/config/MailSenderConfig.java
@@ -10,7 +10,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-@PropertySource("classpath:config/application-smtp.properties")
+@PropertySource("classpath:application-smtp.properties")
 public class MailSenderConfig {
 
     @Value("${spring.mail.host}")
